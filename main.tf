dad94df731eae84b3455d0876ed35a3e0411f74b
TeddyCom1 <csclong@gmail.com>
2026-05-25 16:45:22 +1000

    reverting factory image

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "proxmox_download_file" "talos_image" {
   datastore_id            = "local"
   node_name               = var.proxmox_node_name
   url                     = "https://factory.talos.dev/image/${var.talos_image_factory_id}/v${var.talos_version}/nocloud-amd64.raw.xz"
-  decompression_algorithm = "zst"
+  decompression_algorithm = "xz"
   file_name               = "talos-v${var.talos_version}-nocloud-amd64.img"
   overwrite               = false
 }
